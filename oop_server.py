cb5f1db71a58707015badd5adad6355ab63feee6
eng0waleed <eng.0waleed@gmail.com>
2023-03-03 00:45:42 +0300

    edited oop server

--- oop_server.py
+++ oop_server.py
@@ -21,28 +21,9 @@ class ClientThread(threading.Thread):
 
     def run(self):
         print("Connection from : ", self.clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-        # msg = ''
-        # while True:
-        #     data = self.csocket.recv(2048)
-        #     msg = data.decode()
-        #     if msg == 'bye':
-        #       break
-        #     print("from client", msg)
-        #     self.csocket.send(bytes(msg, 'UTF-8'))
         self.receive_messages()
-        print("Client at ", self.clientAddress, " disconnected...")
 
     def format_message(self, dest_id, tag, message):
-        """formatting the message to be "dest_idMy_id(tag-1/1)message\0" with length of 256 bytes  
-            
-            Args:
-            dest_id (String): the id of the destination
-            tag (String): the tag refer to the type of the message
-            message (String): the message itself
-            
-            return: list of generated messages
-        """
         message_length = len(message)
         dest_id_length = len(dest_id)
         tag_length = len(tag)
@@ -89,23 +70,18 @@ class ClientThread(threading.Thread):
             temp_message = "{}{}{}{}".format(
                 dest_id, my_client_id, "({}-{}/{})".format(tag, 1, 1).ljust(12, '\0'), message)
             messages.append(temp_message)
-
         return messages
 
     def handle_quit(self, clientId):
         for c in test_clients:
-            if(c["clientID"] == clientId):
-                print(test_clients)
+            if(c.clientID == clientId):
                 test_clients.remove(c)
-                print(test_clients)
 
     def add_alive_timestamp(self, contact):
         print(int(contact.replace("\x00", "").strip()))
         for c in test_clients:
             if(c.clientID == int(contact.replace("\x00","").strip())):
-                print(c.timeStamp)
                 c.timeStamp = time.time()
-                print(c.timeStamp)
 
     def send_to_client(self, c, message):
         if(c != None):
@@ -122,13 +98,6 @@ class ClientThread(threading.Thread):
             self.handle_received_message(message.decode())
 
     def handle_received_message(self, message):
-        """collect messages and process them
-
-            Args:
-            message (String): the received message
-
-            return: nothing
-        """
         #@Todo: handle duplicated client_id
         print(message)
         msg_dest_id = message[0:8]
@@ -152,12 +121,11 @@ class ClientThread(threading.Thread):
                     # dest_address = c.clientAddress
                     self.send_to_client(c,message)
 
-        elif(msg_tag == "@List"):
+        elif(msg_tag == "List"):
             contacts = ""
             for c in test_clients:
                 contacts += str(c.clientID).ljust(8, '\0')
-            list_message = self.format_message(
-                msg_source_id, "List", contacts)[0]
+            list_message = self.format_message(msg_source_id, "List", contacts)[0]
             self.send_to_client(None,list_message)
 
         elif(msg_tag == "Alive"):
@@ -167,14 +135,6 @@ class ClientThread(threading.Thread):
             self.handle_quit(msg_source_id.strip())
             
 
-
-LOCALHOST = "127.0.0.1"
-PORT = 8080
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((LOCALHOST, PORT))
-print("Server started")
-print("Waiting for client request..")
 
 
 def remove_offline_clients():
@@ -194,9 +154,22 @@ def set_remove_offline_contacts_interval():
     return created_thread
 
 
-set_remove_offline_contacts_interval()
-while True:
-    server.listen(1)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientAddress, clientsock)
-    newthread.start()
+def listen_to_connections(server):
+    while True:
+        server.listen(1)
+        clientsock, clientAddress = server.accept()
+        newthread = ClientThread(clientAddress, clientsock)
+        newthread.start()
+
+
+if __name__ == '__main__':
+    LOCALHOST = "127.0.0.1"
+    PORT = 8080
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((LOCALHOST, PORT))
+    print("Server started")
+    print("Waiting for client request..")
+
+    set_remove_offline_contacts_interval()
+    listen_to_connections(server)
